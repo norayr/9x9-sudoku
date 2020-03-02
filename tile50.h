7b17662fb930acebef5cc9d28c9d8eae48dd86cd
norayr chilingarian <norayr@arnet.am>
2020-03-02 20:28:42 +0000

    removed unnecessary files?

--- tile50.h
+++
@@ -1,5 +0,0 @@
-extern int tile50_width;
-extern int tile50_height;
-extern int tile50_bytes;
-
-extern const unsigned char tile50_pixel_data[ 50 * 50 * 3 + 1 ];
--- up64.h
+++
@@ -1,5 +0,0 @@
-extern int up64_width;
-extern int up64_height;
-extern int up64_bytes;
-
-extern const unsigned char up64_pixel_data[ 64 * 64 * 3 + 1 ];
