9dac840ceaeb1516ecb011ecd1b6031f1f32dcfc
norayr chilingarian <norayr@arnet.am>
2020-03-02 20:26:43 +0000

    eliminated warnings about unnecessary dependencies and added a couple of c files that were not added for some reason.

---
+++ down64.h
@@ -0,0 +1,5 @@
+extern int down64_width;
+extern int down64_height;
+extern int down64_bytes;
+
+extern const unsigned char down64_pixel_data[ 64 * 64 * 3 + 1 ];
---
+++ up64.h
@@ -0,0 +1,5 @@
+extern int up64_width;
+extern int up64_height;
+extern int up64_bytes;
+
+extern const unsigned char up64_pixel_data[ 64 * 64 * 3 + 1 ];
